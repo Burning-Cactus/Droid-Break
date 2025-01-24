e367e12507ffc96629499a568adfefddf372dcd9
Burning Cactus <23108473+Burning-Cactus@users.noreply.github.com>
2025-01-23 23:20:15 -0600

    Merge two uneven codebases

--- gui/title_screen.gd
+++ gui/title_screen.gd
@@ -1,3 +1,13 @@
 extends Node
 
 @onready var startText: Label = $PanelContainer/MarginContainer/StartText
+var simultaneous_scene = preload("res://Maps/base.tscn").instantiate()
+func _process(delta):
+	
+	if Input.is_action_just_pressed("EnterKey"):
+		_add_a_scene_manually()
+func _add_a_scene_manually():
+	# This is like autoloading the scene, only
+	# it happens after already loading the main scene.
+	get_tree().root.add_child(simultaneous_scene)
+	get_node("PanelContainer").free()
