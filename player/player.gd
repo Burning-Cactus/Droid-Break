e4a89ad2575b099cdd878a7358b85e5898c33e86
Burning Cactus <23108473+Burning-Cactus@users.noreply.github.com>
2025-01-23 21:05:30 -0600

    Basic hitbox and hurtbox set up
    With this, it should be easier to add enemies.

--- player/player.gd
+++ player/player.gd
@@ -2,25 +2,26 @@ extends CharacterBody2D
 
 
 @export var stats: Health
-
 const SPEED: float = 300.0
 const JUMP_VELOCITY: float = -450.0
-
 const MAX_AIR_JUMPS: int = 1
 var air_jumps: int = MAX_AIR_JUMPS
 
 enum {GROUND, FALLING, JUMPING, SLIDING, ON_WALL, WALL_JUMPING}
-
 var state = GROUND
 
 # -1 is left, 1 is right
 var facing: int = 1
 
 signal health_changed(newValue: int)
-
 signal health_reached_zero()
 
 func _ready() -> void:
+	health_changed.emit(stats.health)
+	$HurtBox.take_damage.connect(_on_take_damage)
+	
+func _on_take_damage(damage: int) -> void:
+	stats.health -= damage
 	health_changed.emit(stats.health)
 
 func _process(delta: float) -> void:
@@ -64,21 +65,22 @@ func _physics_process(delta: float) -> void:
 				velocity.y = JUMP_VELOCITY
 			
 			# Acceleration and deceleration is slower in the air.
-			if direction:
+			if direction != 0:
 				facing = direction
 				velocity.x = move_toward(velocity.x, direction * SPEED, 20)
 			else:
 				velocity.x = move_toward(velocity.x, 0, 20)
 				
 			if velocity.y > 0 && is_on_wall():
-				state = ON_WALL
+				set_on_wall()
 				
 		ON_WALL:
 			if Input.is_action_just_pressed("jump"):
 				state = JUMPING
-				velocity.x = -direction * SPEED
+				velocity.x = facing * SPEED
 				velocity.y = JUMP_VELOCITY * 0.75
-			if direction != 0:
+				# This should probably use a raycast or something else instead of facing checks
+			if direction == facing:
 				state = FALLING
 			velocity += get_gravity() * delta
 			velocity.y = min(velocity.y, 50)
@@ -87,6 +89,11 @@ func _physics_process(delta: float) -> void:
 			pass
 	
 	move_and_slide()
+
+func set_on_wall() -> void:
+	state = ON_WALL
+	facing = -facing
+	print(facing)
 
 func wall_jump() -> void:
 	state = WALL_JUMPING
